b994395092a6dda283987ca572d794dc83dcf6f8
lola swift <lolaswift@users.noreply.github.com>
2022-10-30 14:05:12 +0100

    temporary fix the output path.
    need to look into the code what the proper solution is.

--- webapp/app.py
+++ webapp/app.py
@@ -12,7 +12,7 @@ from PIL import Image
 
 app = FastAPI()
 UPLOAD_FOLDER = "images-input"
-OUTPUT_FOLDER = "images-output"
+OUTPUT_FOLDER = "images-output/"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/images-input", StaticFiles(directory=UPLOAD_FOLDER), name="images-input")
 app.mount("/images-output", StaticFiles(directory=OUTPUT_FOLDER), name="images-output")
